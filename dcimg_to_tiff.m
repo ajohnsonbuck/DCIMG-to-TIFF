dd63b49f609dbfd4b1027c26ce150e556790f8d5
Alex Johnson-Buck <alebuck@umich.edu>
2025-07-07 16:02:29 -0400

    Add progress bar showing progress through each TIF file; improve logging.

--- dcimg_to_tiff.m
+++ dcimg_to_tiff.m
@@ -1,4 +1,10 @@
-function dcimg_to_tiff(InputFile,OutputFile,HorizontalFlip)
+function dcimg_to_tiff(InputFile,OutputFile,HorizontalFlip,WaitBarMsg)
+% Args
+% InputFile: string or char of input path + file to DCIMG file
+% OutputFile: string or char of output path + file for TIFF file
+% HorizontalFlip: boolean specifying whether to flip image on horizontal
+% axis
+% WaitBarMsg: message for progress bar
 
 InputFile = char(InputFile);
 
@@ -7,16 +13,22 @@ InputFile = char(InputFile);
 TiffOut = Tiff(OutputFile,"w8");
 TagStruct = createTiffTag(Height, Width);
 
+wb = waitbar(0,WaitBarMsg);
+
 for n = 1:NFrames
     Frame = dcimg_read_frame(InputFile, n);
     if HorizontalFlip == true
         Frame = fliplr(Frame);
     end
     
+    waitbar(n/NFrames,wb);
+
     setTag(TiffOut,TagStruct);
     write(TiffOut,Frame);
     writeDirectory(TiffOut);
 end
+
+close(wb);
 
 TiffOut.close();
 
